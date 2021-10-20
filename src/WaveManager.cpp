10c8608143ceddbc4878ee00acd17c941d77b6cc
Andrew Lauderback <a195l754@ku.edu>
2021-10-20 18:43:43 -0500

    expanded base

--- src/WaveManager.cpp
+++ src/WaveManager.cpp
@@ -1,26 +1,49 @@
 #include "WaveManager.h"
 
+WaveManager::WaveManager()
+{
+    currentWave = 1;
+    enemyCount = 0;
+}
+
 bool WaveManager::waveOver()
 {
-
+    // This is not how I plan to check in final, but enemies don't exist yet
+    if(enemyCount == 0)
+    {
+        return(true);
+    }
+    return(false);
 }
 
 void WaveManager::beginWave()
 {
-
+    // Spawn enemies
 }
 
 void WaveManager::endWave()
 {
-
+    // Clear gamestate
 }
 
 int WaveManager::getWave()
 {
-
+    return(currentWave);
 }
 
 int WaveManager::getEnemiesRemaining()
 {
-    
+    // This is not how I plan to return in final, but enemies don't exist yet
+    return(enemyCount);
+}
+
+int WaveManager::getEnemy(int n)
+{
+    // Blah blah not how blah blah no enemies blah blah
+    if(n<enemies.size())
+    {
+        return enemies.at(n);
+    }
+    // no clue how this is gonna work with enemies, god I wish we used rust
+    return(-1);
 }
--- src/WaveManager.h
+++ src/WaveManager.h
@@ -9,11 +9,14 @@ class WaveManager
         int enemyCount;
         std::vector<int> enemies;
     public:
+        WaveManager();
+
         bool waveOver();
         void beginWave();
         void endWave();
         int getWave();
         int getEnemiesRemaining();
+        int getEnemy(int n);
 };
 
 #endif
