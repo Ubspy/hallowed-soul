2f481f53c1bdec1a463ce216cddcb897f674b680
Andrew Riachi <brasswood@users.noreply.github.com>
2021-10-22 12:33:21 -0500

    document onDraw() and onCollision()

--- src/Entity.h
+++ src/Entity.h
@@ -15,11 +15,36 @@ class Entity
         void update();
 
         // Pure virtual functions needs to be overriden
+
+        /** Called when the collision manager has detected a collision between
+         * this entity and another entity.
+         * 
+         * Examples of things to do in here:
+         *   - Update the velocity of the entity
+         *   - Set the sprite to something different
+         *     (or mark a state change of this Entity so that onDraw can set the sprite)
+         * 
+         * Examples of things to NOT do in here:
+         *   - Draw your sprite to the window.draw()
+         * 
+         * @param hitEntity The entity being collided with. ONLY GUARANTEED TO BE VALID
+         *   FOR THE LIFETIME OF THIS FUNCTION.
+         */
         virtual void onCollision(Entity* hitEntity) = 0; 
 
         // Virtual functions for basic entity actions that can be overridden
         virtual void spawn(sf::Vector2<int> spawnLocation);
         virtual void kill();
+
+        /** Called before the Entity is drawn to the screen.
+         * 
+         * Things to do in here:
+         *   - Update the animation frame that your sprite is on
+         *   - Change your sprite's position in the world using sprite.setPosition()
+         * 
+         * Things to NOT do in here (for now):
+         *   - Actually draw your sprite to the window using the window.draw() method
+         */
         virtual void onDraw();
 
     protected:
